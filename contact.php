0a279b9be642d34051076ed6df5435ff193a145a
Lyov Khachatryan <lyovkhachatryanid@gmail.com>
2018-06-04 01:37:24 +0400

    Init

---
+++ contact.php
@@ -0,0 +1,52 @@
+<?php
+
+	function sendMail($name, $email, $comment) {
+		$result = [];
+		$error = false;
+		if (strlen($name) <= 3) {
+			$error = 'Name is too short';
+		} else if (strlen($comment) <= 10) {
+			$error = 'Message is too short';
+		} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$error = 'Email is invalid';
+		}
+
+		if ($error){
+			$result['success'] = false;
+			$result['message'] = $error;
+		} else {
+
+			$message = 'Name: ' . $name . "\r\n";
+			$message.=  'Email: ' . $email . "\r\n";
+			$message.=  'Message: ' . $comment;
+
+			$headers = 'From: no-reply@theplace.am' . "\r\n";
+			$headers.= 'Reply-To: ' . $email . "\r\n" ;
+			
+			$sent = mail($to, $subject, $message, $headers);
+
+			if ($sent) {
+				$result['message'] = 'Thank you!';
+				$result['success'] = true;
+			} else {
+				$result['message'] = 'Please try again later';
+				$result['success'] = false;
+			}
+		}	
+
+		
+
+		return $result;
+	}
+	
+
+	
+	$email_to = 'andranik@hovesyan.pro';
+	$subject = 'The Place Contact';
+
+	if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['comment'])) {
+
+		$result = sendMail($_POST['name'], $_POST['email'], $_POST['comment']);
+
+		echo json_encode($result);
+	}
---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+$(function () {
+	
+
+    $('.nav-link').on('click', function (e) {
+      $('.nav-link').removeClass('active');
+      $(this).addClass('active');
+      var id = $(this).attr('data-content');
+      $('.content').addClass('inactive');
+      $('#' + id).removeClass('inactive');
+    });
+
+    $(document).on('submit','#contactForm',function(e) {
+      e.preventDefault();    
+      var data = $(this).serialize();
+      $.post('/contact.php', data, function(res) {
+        var data = JSON.parse(res);
+        console.log(data);
+        var $alert = $('#contactFormAlert');
+        if (data.success) {
+          $alert.addClass('alert-success');
+        } else {
+          $alert.addClass('alert-danger');
+        }
+        $alert.text(data.message).fadeIn();
+      });
+        
+        
+        
+        
+    })
+    
+
+
+})
+
+
